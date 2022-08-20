6da54ac65e1b5b0816106fff8b7969a690e684df
WeixiangCao <piedra163@163.com>
2022-08-20 15:14:09 +0800

    router完成

---
+++ src/views/employees/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>employ</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/social/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>sociation</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
